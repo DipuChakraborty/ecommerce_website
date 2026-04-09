db3cb82942d40e5dfa24894cfaf39dc541ba07a2
DipuChakraborty <116711343+DipuChakraborty@users.noreply.github.com>
2026-04-09 21:55:46 +0600

    Refactor ALLOWED_HOSTS environment variable handling

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
+hosts = os.getenv("DJANGO_ALLOWED_HOSTS") or os.getenv("ALLOWED_HOSTS") or ""
+ALLOWED_HOSTS = hosts.split(",")
 
 
 # Application definition
